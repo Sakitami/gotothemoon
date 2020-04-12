e4afb8bcc26d6d05f24c3d58243ce4d0228a34d8
Sakitami <sakitam2373@gmail.com>
2020-04-12 14:53:27 +0800

    Update

--- p1-helloworld/min.go
+++ p1-helloworld/min.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+    // 声明一个变量，并初始化(没有默认类型，类型自动匹配)
+    var a = "Sakitami"
+    // 声明一个变量及其类型（没有初始化，默认为零值）
+    var b int
+    var c bool
     fmt.Println("Hello,World!")
+    /* 输出结果：
+       Sakitami 0 false */
+    fmt.Println(a,b,c)
 }
 
 
